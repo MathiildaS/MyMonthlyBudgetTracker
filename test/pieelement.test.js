a92afd1d68ce0f032f104593eb180fe9954a1a92
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-12 19:26:02 +0200

    Add test to set warning and dangerous boundaries of pie

--- test/pieelement.test.js
+++ test/pieelement.test.js
@@ -3,13 +3,11 @@ import { describe, it, expect, beforeEach, vi } from "vitest"
 import "../src/js/components/pie/index.js"
 
 let pieElement
-
 describe("test pie-element component", () => {
   beforeEach(() => {
     document.body.innerHTML = ""
 
     pieElement = document.createElement("pie-element")
-    document.body.appendChild(pieElement)
   })
 
   it("should be initialized with base value", () => {
@@ -20,5 +18,17 @@ describe("test pie-element component", () => {
     pieElement.pieRender = { createSlice: vi.fn() }
     pieElement.displaySliceOnPieBasedOnInput(100)
     expect(pieElement.pieRender.createSlice).toHaveBeenCalledWith(100)
+  })
+
+  it("should change colour of pie", () => {
+    pieElement.pieRender = { setPieColour: vi.fn() }
+    pieElement.setColourOfPie('#FFFFFF')
+    expect(pieElement.pieRender.setPieColour).toHaveBeenCalledWith('#FFFFFF')
+  })
+
+  it("should set warning and dangerous boundaries of pie", () => {
+    pieElement.pieRender = { setPieBoundaries: vi.fn() }
+    pieElement.setWarningAndDangerBoundariesForPie(25, 10)
+    expect(pieElement.pieRender.setPieBoundaries).toHaveBeenCalledWith(25, 10)
   })
 })
