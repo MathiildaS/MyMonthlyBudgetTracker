3ae6da19a3c0df0554200905597b3542a737ee55
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-14 14:12:01 +0200

    Att method to display remaining of budget after added expense

--- src/js/components/food-budget/food-budget-element.js
+++ src/js/components/food-budget/food-budget-element.js
@@ -63,28 +63,61 @@ button:hover {
     background-color: #9fa0d6;
 }
 
+.expenses-remaining {
+display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 1rem;
+  row-gap: .25rem;
+  align-items: start;
+}
+
+.expenses-remaining p {
+  margin: 0;
+  line-height: 1.5;
+}
+
+#expenses {
+grid-row: 1;
+}
+
+#expensesValue {
+grid-row: 2;
+}
+
+#remaining {
+grid-row: 1;
+}
+
+#remainingValue {
+grid-row: 2;
+}
+
 @media (max-width: 1200px) {
   .main {
   grid-template-columns: 1fr 1.5fr 1fr;
   grid-template-areas: 'left middle right';
   }
+}
 
 @media (max-width: 1000px) {
   .main {
   grid-template-columns: 0.5fr 1fr 0.6fr;
   grid-template-areas: 'left middle right';
   }
+}
 
 @media (max-width: 600px) {
   .main {
   grid-template-columns: 1fr;
   grid-template-areas: 'left' 'middle' 'right';
   }
+}
 
 @media (max-width: 400px) {
   .main {
   gap: 0.5rem;
   }
+}
 </style>
 <div class='main'>
 <section class='left'>
@@ -107,7 +140,12 @@ button:hover {
 <h1><span id='budgetYear'>—</span></h1>
     <h2><span id='budgetMonth'>—</span></h2>
     <p><strong>Budget:</strong> <span id='budgetValue'>—</span></p>
-    <p><strong>Expenses:</strong></p><ul id='expensesList'></ul>
+    <div class="expenses-remaining">
+    <p id="expenses"><strong>Expenses:</strong></p>
+    <p id="expensesValue">—</p>
+    <p id="remaining"><strong>Remaining:</strong></p>
+    <p id="remainingValue">—</p>
+    </div>
 </section>
 </div>
 `
@@ -138,11 +176,13 @@ customElements.define(
       this.currentBudget = this.shadowRoot.querySelector('#budgetValue')
       this.currentYear = this.shadowRoot.querySelector('#budgetYear')
       this.currentMonth = this.shadowRoot.querySelector('#budgetMonth')
-      this.allAddedExpenses = this.shadowRoot.querySelector('#expensesList')
+      this.allAddedExpenses = this.shadowRoot.querySelector('#expensesValue')
+      this.remainingOfBudget = this.shadowRoot.querySelector('#remainingValue')
 
       this.addedBudget
       this.addedExpense
       this.collectedExpenses = []
+      this.remainingValue
     }
 
     connectedCallback() {
@@ -158,8 +198,8 @@ customElements.define(
         this.addedExpense = Number(event.detail.expense)
         this.addExpenseToCollection()
         this.displayAddedExpenses()
+        this.displayRemainingBudgetAfterAddedExpense()
         this.pieElement.displaySliceOnPieBasedOnInput(this.addedExpense)
-        console.log(this.pieElement.getStateOfPie())
       })
 
       this.pieButton.addEventListener('click', () => {
@@ -204,9 +244,9 @@ customElements.define(
       this.allAddedExpenses.replaceChildren()
 
       this.collectedExpenses.forEach((expense) => {
-        const liElement = document.createElement('li')
-        liElement.textContent = expense
-        this.allAddedExpenses.appendChild(liElement)
+        const pElement = document.createElement('p')
+        pElement.textContent = expense
+        this.allAddedExpenses.appendChild(pElement)
       })
     }
 
@@ -217,6 +257,19 @@ customElements.define(
     drawAddedExpensesOnPie() {
       this.collectedExpenses.forEach((addedExpense) => {
         this.pieElement.displaySliceOnPieBasedOnInput(addedExpense)
+      })
+    }
+
+    displayRemainingBudgetAfterAddedExpense() {
+      this.remainingOfBudget.replaceChildren()
+
+      this.remainingValue = this.addedBudget
+
+      this.collectedExpenses.forEach((expense) => {
+        this.remainingValue -= expense
+        const pElement = document.createElement('p')
+        pElement.textContent = this.remainingValue
+        this.remainingOfBudget.appendChild(pElement)
       })
     }
   }
