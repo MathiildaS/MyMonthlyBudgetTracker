bcd9b4b7d85f0ea34aa338ce51a1712a3222836c
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-15 15:46:17 +0200

    Insert select tag to select currency and att to detail in budgetAdded custom event

--- src/js/components/budget-form/budget-form-element.js
+++ src/js/components/budget-form/budget-form-element.js
@@ -22,14 +22,58 @@ button:hover {
     background-color: #9fa0d6;
 }
 
-input {
-  font-family: 'DynaPuff';
-  padding: 0.5rem 1rem;
+  input, select {
+    font-family: 'DynaPuff';
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    border-radius: 4px;
+  }
+
+  #budgetForm {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    grid-template-areas:
+      "budget currency"
+      "submit submit";
+    gap: 0.5rem;
+    align-items: stretch;
+  }
+
+#budget { 
+grid-area: budget; 
+width: 100%; 
 }
+
+#currency { 
+grid-area: currency; 
+}
+  #budgetForm button { 
+  grid-area: submit; 
+  width: 100%; 
+  }
+
+  #currency {
+    background-color: #b0a8d6b9;
+    color: #ffffff;
+    border: none;
+    cursor: pointer;
+  }
+  #currency:hover { 
+  background-color: #9fa0d6; 
+  }
 </style>
 <div>
 <form id='budgetForm'>
 <input type='text' name='budget' placeholder='Add this months budget' required />
+<select name="currency" id="currency" required>
+<option value="KR">SEK</option>
+<option value="$">USD</option>
+<option value="€">EUR</option>
+<option value="£">GBP</option>
+<option value="A$">AUD</option>
+<option value="CN¥">CNY</option>
+<option value="JP¥">JPY</option>
+</select>
 <button type='submit'>ADD BUDGET</button>
 </form>
 </div>
@@ -63,14 +107,16 @@ customElements.define(
     collectFormDataAndSendBudget() {
       const formData = new FormData(this.form)
       const budget = formData.get('budget')
-      this.sendAddedBudget(budget)
+      const currency = formData.get('currency')
+      this.sendAddedBudget(budget, currency)
       this.form.reset()
     }
 
-    sendAddedBudget(budget) {
+    sendAddedBudget(budget, currency) {
       const budgetAdded = new CustomEvent('budgetAdded', {
         detail: {
           budget: budget,
+          currency: currency,
         },
         bubbles: true,
       })
