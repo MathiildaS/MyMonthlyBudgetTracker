b55743d25a4717b50608df9851859ae71a4f1e41
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-16 22:43:16 +0200

    Add methods to parse value, trim and remove whitespaces

--- src/js/utils/parser.js
+++ src/js/utils/parser.js
@@ -1,16 +1,25 @@
 /**
- * @file A module with methods to parse inputs and other.
+ * @file A module with methods to parse values.
  * @author Mathilda Segerlund <ms228qs@student.lnu.se>
  * @version 1.0.0
  */
 export class Parser {
-  parseInputToNumber(input) {
-if ()
-    throw new Error("")
+
+  // Trim and remove whitespaces before parsing to number.
+  parseValueToNumber(value) {
+    const trimmedValue = String(value).trim()
+    const wsRemoved = trimmedValue.replace(/\s+/g, '')
+    const parsedValue = Number(wsRemoved)
+
+    return parsedValue
   }
 
-    parseInputToString(input) {
+  // Parse value to string, trim and remove whitespaces.
+  parseValueToString(value) {
+    const parsedToString = String(value)
+    const trimmedString = parsedToString.trim()
+    const stringValue = trimmedString.replace(/\s+/g, '')
 
-    throw new Error("")
+    return stringValue
   }
 }
