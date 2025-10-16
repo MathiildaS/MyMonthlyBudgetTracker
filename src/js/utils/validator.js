dae6f03a6b518060c4f6047bd733bcce9664e73d
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-16 22:42:32 +0200

    Remove validateInputExist method

--- src/js/utils/validator.js
+++ src/js/utils/validator.js
@@ -5,38 +5,36 @@
  */
 export class Validator {
 
-  validateNumberInput(input) {
-    if (typeof input !== "number" || !Number.isFinite(input)) {
+  validateNumber(value) {
+    if (typeof value !== "number" || !Number.isFinite(value)) {
       throw new Error("The value must be a number, not positive Infinity, not negative Infinity or NaN.")
     }
-    if (input <= 0) {
+    if (value <= 0) {
       throw new Error("The value must be larger than or zero.")
     }
-    if (Number.isNaN(input)) {
+    if (Number.isNaN(value)) {
       throw new Error('The value cannot be NaN.')
     }
-    if (typeof input === 'string') {
+    if (typeof value === 'string') {
       throw new Error('The value must not be a string.')
     }
-    if (input === null || input === undefined) {
+    if (value === null || value === undefined) {
       throw new Error('The value is missing or undefined.')
     }
-    if (input > 10000000) {
+    if (value > 10000000) {
       throw new Error("The value must be less than 10.000.000")
     }
   }
 
-  validateStringInput(input) {
-    if (typeof input !== "string") {
+  validateString(value) {
+    if (typeof value !== "string") {
       throw new Error("The value must be a string.")
     }
-    if (input === null || input === undefined) {
+    if (value === null || value === undefined) {
       throw new Error('The value is missing or undefined.')
     }
-  }
-
-  validateInputExist(input) {
-    if (!input)
-      return
+    if (typeof value === 'string' && value.trim() === '') {
+      throw new Error('The value must not be empty.')
+    }
   }
 }
