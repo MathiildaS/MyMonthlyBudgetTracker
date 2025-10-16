e5fda002e7a3775fe755c205776a9fe4def10234
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-16 22:16:35 +0200

    Add validateNumberInput method and validation to ensure valid number, not less than 0 or greater than 10000000

--- src/js/utils/validator.js
+++ src/js/utils/validator.js
@@ -10,7 +10,19 @@ export class Validator {
       throw new Error("The value must be a number, not positive Infinity, not negative Infinity or NaN.")
     }
     if (input <= 0) {
-      throw new Error("The value must be larger than zero")
+      throw new Error("The value must be larger than or zero.")
+    }
+    if (Number.isNaN(input)) {
+      throw new Error('The value cannot be NaN.')
+    }
+    if (typeof input === 'string') {
+      throw new Error('The value must not be a string.')
+    }
+    if (input === null || input === undefined) {
+      throw new Error('The value is missing or undefined.')
+    }
+    if (input > 10000000) {
+      throw new Error("The value must be less than 10.000.000")
     }
   }
 
