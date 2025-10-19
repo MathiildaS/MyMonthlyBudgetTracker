04be38fe12c79ba823cdceafaad28017b44b96bc
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-10-19 17:05:46 +0200

    Import daily-allowance and add HTML structure

--- src/js/components/daily-allowance/daily-allowance.html.js
+++ src/js/components/daily-allowance/daily-allowance.html.js
@@ -7,4 +7,10 @@
 export const htmlTemplate = document.createElement('template')
 
 htmlTemplate.innerHTML = `
+<div class="dailyAllowance">
+  <h2>Daily Allowance</h2>
+  <p>This month you can spend<br>
+  <span id="allowance"></span><br>
+  to stay within your budget.</p>
+</div>
 `
